54a21ccd762d65976bd3fac107ca0c1ed0bf4299
FizzyApple12 <davideroeder@gmail.com>
2021-06-29 01:50:34 -0400

    inital commit

---
+++ FizzyUtils/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace FizzyUtils {
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin {
+        internal const string Name = "FizzyUtils";
+        public static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        
+
+        UsageTracker.UsageTrackerUser usageTrackerUser;
+
+        [Init]
+        public Plugin(IPALogger logger) {
+            Instance = this;
+            Log = logger;
+
+            Utils.usageTracker = new UsageTracker("127.0.0.1");
+            usageTrackerUser = Utils.usageTracker.AddUser(Name);
+        }
+
+        [OnExit]
+        public void OnApplicationQuit() {
+            Utils.usageTracker.Stop();
+        }
+    }
+
+    public static class Utils {
+        public static UsageTracker usageTracker;
+    }
+}
---
+++ FizzyUtils/UsageTracker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebSocketSharp;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace FizzyUtils {
+    public class UsageTracker {
+        private string url;
+
+        private WebSocket webSocket = null;
+
+        private bool reconnect = true;
+
+        private List<UsageTrackerUser> usageTrackerUsers = new List<UsageTrackerUser>();
+
+        internal UsageTracker(string url) {
+            this.url = url;
+
+            webSocket = new WebSocket($"ws://{url}/usagetracker");
+            webSocket.OnMessage += OnMessage;
+            webSocket.OnClose += OnClose;
+            webSocket.ConnectAsync();
+        }
+
+        private void OnClose(object sender, CloseEventArgs e) {
+            if (reconnect) webSocket.ConnectAsync();
+        }
+
+        private void OnMessage(object sender, MessageEventArgs e) {
+            if (!e.IsText) return;
+
+            Console.WriteLine(e.Data);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            StringWriter stringWriter = new StringWriter(stringBuilder);
+            JsonWriter writer = new JsonTextWriter(stringWriter);
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("event");
+            writer.WriteValue("ping");
+
+            writer.WritePropertyName("mods");
+            writer.WriteStartArray();
+
+            foreach (UsageTrackerUser usageTrackerUser in usageTrackerUsers) {
+                writer.WriteValue(usageTrackerUser.modName);
+            }
+
+            writer.WriteEnd();
+            writer.WriteEndObject();
+
+            webSocket.SendAsync(stringBuilder.ToString(), (bool completed) => { });
+        }
+
+        public UsageTrackerUser AddUser(string modName) {
+            UsageTrackerUser usageTrackerUser = new UsageTrackerUser(modName);
+
+            usageTrackerUsers.Add(usageTrackerUser);
+
+            return usageTrackerUser;
+        }
+
+        internal void RemoveUser(Guid uuid) {
+            for (int i = 0; i < usageTrackerUsers.Count; i++) {
+                if (usageTrackerUsers[i].uuid.Equals(uuid)) {
+                    usageTrackerUsers.RemoveAt(i);
+                    i--;
+                }
+            } 
+        }
+
+        internal void Stop() {
+            reconnect = false;
+            webSocket.Close();
+        } 
+
+        public class UsageTrackerUser {
+            internal string modName;
+            internal Guid uuid;
+            internal UsageTracker parent;
+
+            internal UsageTrackerUser(string modName) {
+                uuid = Guid.NewGuid();
+                this.modName = modName;
+            }
+
+            public void Remove() {
+                parent.RemoveUser(uuid);
+            }
+        }
+    }
+}
